417f62945918fdb57f649b664bae6e2575ef54ff
Nidhi <nidhi@vpn-075.umiacs.umd.edu>
2018-02-22 10:41:44 -0500

    Added URLs to download files and updated Metadata information

--- inst/scripts/make_silva128.R
+++ inst/scripts/make_silva128.R
@@ -6,27 +6,34 @@ library(metagenomeFeatures) ## Needed for the make_mgdb_sqlite function
 library(tidyr)
 library(tidyverse)
 library(stringr)
+library(R.utils)
 ## Database URL
-db_root_url <- "ftp://greengenes.microbio.me/greengenes_release/gg_13_8_otus/"
-taxa_url <- paste0(db_root_url, "taxonomy/97_otu_taxonomy.txt")
-aligned_seq_url <- paste0(db_root_url, "rep_set_aligned/97_otus.fasta")
-seq_url <- paste0(db_root_url, "rep_set/97_otus.fasta")
-tree_url <- paste0(db_root_url, "trees/97_otus_unannotated.tree")
+db_root_url <- "https://www.arb-silva.de/fileadmin/silva_databases/release_128/Exports/"
+taxa_url <- paste0(db_root_url, "taxonomy/taxmap_slv_ssu_ref_128.txt.gz")
+taxmap_url <- paste0(db_root_url, "taxonomy/tax_slv_ssu_128.txt")
+aligned_seq_url <- paste0(db_root_url, "SILVA_128_SSURef_tax_silva_full_align_trunc.fasta.gz")
+seq_url <- paste0(db_root_url, "SILVA_128_SSURef_tax_silva.fasta.gz")
+tree_url <- paste0(db_root_url, "taxonomy/tax_slv_ssu_128.tre")
+rnacentral_url <- "ftp://ftp.ebi.ac.uk/pub/databases/RNAcentral/releases/8.0/id_mapping/database_mappings/silva.tsv"
 
 ## Downloaded files
 taxa_file <- tempfile()
+taxagz_file <- tempfile()
 seq_file <- tempfile()
-tree_file <- "../extdata/gg13.8_97.tre"
-rnacentral_file <- "../../rnacentral/greengenes.tsv"
+seqgz_file <- tempfile()
+rnacentral_file <- tempfile()
+taxmap_file <- tempfile()
+tree_file <- "../extdata/silva128.tre"
 
 ## MD5 check sums from initial download
-taxa_md5 <- "56ef15dccf2e931ec173f4f977ed649b"
-seq_md5 <- "50b2269712b3738afb41892bed936c29"
-tree_md5 <- "e42c0cb6e4eaf641742cd5767c4a0101"
+taxa_md5 <- "766e219360430c7117fd6d76a4525b04"
+taxmap_md5 <- "fe5ff3ce9681edc8cf523a4706e76243"
+seq_md5 <- "e6feea458c5f1194192c0710c0f40938"
+tree_md5 <- "81fe22e0f57a478bda5cea67857baade"
 
 ## MgDb database files name
-db_file <- "../extdata/silva.sqlite"
-metadata_file <- "../extdata/silva_metadata.RData"
+db_file <- "../extdata/silva128.sqlite"
+metadata_file <- "../extdata/silva128_metadata.RData"
 
 ### Download database files ####################################################
 download_db <- function(url, file_name, md5){
@@ -39,20 +46,21 @@ download_db <- function(url, file_name, md5){
 }
 
 ## Taxa Data
-download_db(taxa_url, taxa_file, taxa_md5)
+download_db(taxa_url, taxagz_file, taxa_md5)
+gunzip(filename = taxagz_file, destname = taxa_file)
+
+## Taxonomy mapping Data
+download_db(taxmap_url, taxmap_file, taxmap_md5)
 
 ## Seq Data
-download_db(seq_url, seq_file, seq_md5)
+download_db(seq_url, seqgz_file, seq_md5)
+gunzip(filename = seqgz_file, destname = seq_file)
 
 ## Tree Data
 download_db(tree_url, tree_file, tree_md5)
 
-##Directly accessing the files locally
-taxa_file <- "../../silva_db/taxmap_slv_ssu_ref_128.txt"
-seq_file <- "../../silva_db/SILVA_128_SSURef_tax_silva.fasta"
-tree_file <- "../../silva_db/tax_slv_ssu_128.tre"
-taxmap_file <- "../../silva_db/tax_slv_ssu_128.txt"
-rnacentral_file <- "../../silva_db/silva.tsv"
+##Directly downloading RNAcentral mapping file
+download.file(rnacentral_url, rnacentral_file)
 
 ### Create SQLite DB with Taxa and Seq Data ####################################
 expand_path <- function(path){
@@ -84,13 +92,12 @@ parse_silva <- function(taxa_file, rnacentral_file, taxamap_file){
     taxa_df <- data.frame(wide_taxa_df$primaryAccession,wide_taxa_df$domain,wide_taxa_df$phylum,wide_taxa_df$class, wide_taxa_df$order,wide_taxa_df$family, wide_taxa_df$genus)
     taxa_df <- taxa_df[match(taxa$primaryAccession[1:50], taxa_df$wide_taxa_df.primaryAccession),]
     taxa_df <- cbind(taxa_df, taxa$organism_name[1:50])
-    colnames(taxa_df) <- c("Keys","Kingdom","Phylum","Class","Order","Family","Genus","Species")
+    colnames(taxa_df) <- c("Keys","Kingdom","Phylum","Class","Ord","Family","Genus","Species")
     ## Add RNAcentral ID and NCBItax ID - The primaryAccession field in taxa_df differs from RNAmap identifier.
     # rnamap <- read.delim(rnacentral_file, stringsAsFactors = FALSE, header = FALSE)
     # rnamap_new <- rnamap[match(taxa$primaryAccession,rnamap$V3),]
     # taxa_df <-cbind(taxa_df,rnamap_new$V1[1:50],rnamap_new$V4[1:50])
     # colnames(taxa_df) <- c("Keys","Kingdom","Phylum","Class","Ord","Family","Genus","Species","RNAcentralID","NCBItaxID")
-    
     ## Return as a data.frame
     data.frame(taxa_df)
 }
@@ -108,9 +115,9 @@ metagenomeFeatures::make_mgdb_sqlite(db_name = "silva",
 
 ### Database Metadata ##########################################################
 metadata <- list(ACCESSION_DATE = date(),
-                 URL = "ftp://greengenes.microbio.me/greengenes_release/gg_13_8_otus",
-                 DB_TYPE_NAME = "GreenGenes",
-                 DB_VERSION = "13.8 97% OTUS",
+                 URL = "https://www.arb-silva.de/fileadmin/silva_databases/release_128/",
+                 DB_TYPE_NAME = "SILVA",
+                 DB_VERSION = "128 99% OTUS",
                  DB_TYPE_VALUE = "MgDb",
                  DB_SCHEMA_VERSION = "2.0")
 
